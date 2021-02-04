3efc5c22d18108d4642a6c3d118b877b2de9f2a7
John Smith <johnjs1072@gmail.com>
2021-02-05 01:13:08 +0300

    added header

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  //https://www.youtube.com/watch?v=pxWe-nlOSY4
+  return (
+    <div className="home">
+      <div className="home_container">
+        <img
+          className="home_image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/41/TR-hq/2020/img/Prime/XCM_Manual_ORIGIN_1259690_1329040_TR_tr_pv_launch_hero_banners_tr_tr_3347402_1500x600_1X_tr_TR._CB405452713_.jpg"
+          alt="aws banner"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAyh2iwXZdz5I8hOiNtmVEs8RzdpQLE6Q0",
+    authDomain: "my-aws-challenge.firebaseapp.com",
+    projectId: "my-aws-challenge",
+    storageBucket: "my-aws-challenge.appspot.com",
+    messagingSenderId: "155999117263",
+    appId: "1:155999117263:web:dc833e9140854fb19f3cba",
+    measurementId: "G-38QWKQC0Z6"
+  };
