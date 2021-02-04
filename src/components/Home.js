977a38a5cca728b557941e52f35e933efe4556a4
John Smith <johnjs1072@gmail.com>
2021-02-05 01:37:56 +0300

    added Home component image and css

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,6 +11,9 @@ const Home = () => {
           src="https://images-eu.ssl-images-amazon.com/images/G/41/TR-hq/2020/img/Prime/XCM_Manual_ORIGIN_1259690_1329040_TR_tr_pv_launch_hero_banners_tr_tr_3347402_1500x600_1X_tr_TR._CB405452713_.jpg"
           alt="aws banner"
         />
+        <div className="home_raw">
+          {/* product */}
+        </div>
       </div>
     </div>
   );
