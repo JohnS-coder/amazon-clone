efd2bd4f211c85197c9153756be41ca6d64da792
John Smith <johnjs1072@gmail.com>
2021-03-12 00:27:10 +0300

    Links and checkout page added.

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "./Checkout.css";
 
 const Checkout = () => {
-  return <div>This is CHECKOUT</div>;
+  return (
+    <div className="checkout">
+      <div className="checkout_left">
+        <img
+          className="checkout_ad"
+          src="https://www.disruptivestatic.com/wp-content/uploads/2018/10/Screen-Shot-2018-10-29-at-11.50.03-AM-450x96.png"
+          alt="Ad jpg"
+        />
+        <div>
+          <h2 className="checkout_title">Your shopping basket</h2>
+        </div>
+      </div>
+      <div className="checkout_right">
+        <h2>The subtotal will go here</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Checkout;
